bf719c1a27853e445a29a72e9facc4a5b6543b3a
dr1602 <david.arangelg@gmail.com>
2023-05-24 14:44:26 -0600

    Mi primer commit

---
+++ src/TodoCounter.js
@@ -0,0 +1,40 @@
+import React from 'react';
+
+import './TodoCounter.css'; //importar archivo de css, de acuerdo con el curso de frameworks y librerias
+
+/* 
+Una opcion para escribir CSS en react:
+
+const styles = {
+    color: '#E37222',
+    backgroundColor: '#07889B', //en vez de guiones, aplicac cammel case.
+};
+
+function TodoCounter() {
+    return(
+        < h2 style={styles}> Has completado 2 de 3 TODOs </h2>
+    );
+}
+
+// Otra opcion es:
+
+function TodoCounter() {
+    return(
+        < h2 style={const styles = {
+            color: '#E37222',
+            backgroundColor: '#07889B', //en vez de guiones, aplicac cammel case.
+        }}> Has completado 2 de 3 TODOs </h2>
+    );
+}
+
+*/
+
+function TodoCounter() {
+    return(
+        < h2 className='TodoCounter'> Has completado 2 de 3 TODOs </h2>
+    );
+}
+
+// cuando queremos enviar un objeto a una propiedad o atributo o react, tenemos que usar llaves. en este caso se necesitan 2 llaves.
+
+export { TodoCounter };
